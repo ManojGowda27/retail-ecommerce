ba79deb8d5310bd7120463d3642cde80418a3add
MANOJ GOWDA VENKATACHALA MURTHY <manojgowdavenkatachalamurthy@MANOJs-MacBook-Air.local>
2022-09-06 12:57:39 -0400

    added a slider component and transitions

---
+++ src/data.js
@@ -0,0 +1,25 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img:require("./images/5.png"),
+        title: "Summer Sale",
+        description:"No compromise on style! Flat 50 on new arrivals",
+        bg:"#EDFFD9",
+    },
+
+    {
+        id: 2,
+        img:require("./images/7.png"),
+        title: "Summer Sale",
+        description:"No compromise on style! Flat 50 on new arrivals",
+        bg:"#B3DEE2",
+    },
+
+    {
+        id: 3,
+        img:require("./images/6.png"),
+        title: "Summer Sale",
+        description:"No compromise on style! Flat 50 on new arrivals",
+        bg:"#C7D6D5",
+    }
+]
--- src/pages/Landingpage.jsx
+++ src/pages/Landingpage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "../components/Navbar/Navbar";
 import Announcements from "../components/Announcement/Announcements";
+import Slider from "../components/Slider/Slider";
 
 const Landingpage = () => {
   return (
     <div>
       <Announcements />
       <Navbar />
+      <Slider />
     </div>
   );
 };
