6db28d75fe921a8705efeab18353fac6212d942e
MANOJ GOWDA VENKATACHALA MURTHY <manojgowdavenkatachalamurthy@MANOJs-MacBook-Air.local>
2022-11-11 19:17:44 -0500

    Developed and designed Register and Login Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Productlist from "./pages/Productlist";
 import Product from "./pages/Product";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 import Landingpage from "./pages/Landingpage";
 
 const App = () => {
-  return <Product />;
+  return <Login />;
 };
 
 export default App;
